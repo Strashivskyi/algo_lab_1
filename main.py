9f538027f28a65dc8a9733826ffb581a08afc427
Strashivskyi <55955717+Strashivskyi@users.noreply.github.com>
2020-10-26 17:46:48 +0200

    main uses csv file now

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import timeit
-from datetime import timedelta
-
-from Laptop import Laptop, heap_sort_by_CPU_speed_in_GHz_ascending
+import csv_reader
+from Laptop import Laptop, heap_sort_by_CPU_speed_in_GHz_ascending, bubble_sort_by_amount_of_RAM_descending
 
 
 def main():
-    first_laptop = Laptop(14, 16, "Glucklich")
-    second_laptop = Laptop(200, 40, "Henry")
-    third_laptop = Laptop(100, 50, "Kless")
-    laptops = [first_laptop, second_laptop, third_laptop]
-    Laptop.bubble_sort_by_amount_of_RAM_descending(laptops)
+    laptops = csv_reader.csv_to_laptops_converter('laptops1.csv')
+    bubble_sort_by_amount_of_RAM_descending(laptops)
     heap_sort_by_CPU_speed_in_GHz_ascending(laptops)
 
 
