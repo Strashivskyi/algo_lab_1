c352144016db2381edb571927b7c2d9ca4085e9a
Strashivskyi <55955717+Strashivskyi@users.noreply.github.com>
2020-10-26 17:45:26 +0200

    made bubble sort into a function

--- Laptop.py
+++ Laptop.py
@@ -1,4 +1,3 @@
-import timeit
 from datetime import timedelta
 from timeit import default_timer as timer
 import counters
@@ -14,37 +13,37 @@ class Laptop:
         return "== Manufacturer:" + self.manufacturer_name + "; CPU_speed_in_GHz:" + str(
             self.CPU_speed_in_GHz) + "; amount_of_RAM:" + str(self.amount_of_RAM)
 
-    @staticmethod
-    def bubble_sort_by_amount_of_RAM_descending(laptops):
-        start_time = timer()
-        print("---------------------------------------------------")
-        print("BUBBLESORT:\n")
-        n = len(laptops)
-        for i in range(n - 1):
-            has_swapped = 0
-            for j in range(n - 1):
-                counters.bubble_comparison_number = counters.bubble_comparison_number + 2
-                if laptops[j].amount_of_RAM < laptops[j + 1].amount_of_RAM:
-                    laptops[j], laptops[j + 1] = laptops[j + 1], laptops[j]
-                    counters.bubble_swap_number = counters.bubble_swap_number + 1
-                    has_swapped = 1
-                if has_swapped == 0:
-                    break
-        elapsed_time = timedelta(seconds=timer() - start_time)
-        for x in range(len(laptops)):
-            print(laptops[x])
-        print("\nExecution duration:")
-        print(str(elapsed_time)[6:] + "s")
-        print("\nComparisons number:")
-        print(counters.bubble_comparison_number)
-        print("\nSwaps number:")
-        print(counters.bubble_swap_number)
+
+def bubble_sort_by_amount_of_RAM_descending(laptops):
+    print("---------------------------------------------------")
+    print("BUBBLESORT:\n")
+    start_time = timer()
+    n = len(laptops)
+    for i in range(n - 1):
+        has_swapped = 0
+        for j in range(n - 1):
+            counters.bubble_comparison_number = counters.bubble_comparison_number + 2
+            if laptops[j].amount_of_RAM < laptops[j + 1].amount_of_RAM:
+                laptops[j], laptops[j + 1] = laptops[j + 1], laptops[j]
+                counters.bubble_swap_number = counters.bubble_swap_number + 1
+                has_swapped = 1
+            if has_swapped == 0:
+                break
+    elapsed_time = timedelta(seconds=timer() - start_time)
+    for x in range(len(laptops)):
+        print(laptops[x])
+    print("\nExecution duration:")
+    print(str(elapsed_time)[6:] + "s")
+    print("\nComparisons number:")
+    print(counters.bubble_comparison_number)
+    print("\nSwaps number:")
+    print(counters.bubble_swap_number)
 
 
 def heap_sort_by_CPU_speed_in_GHz_ascending(laptops):
-    start_time = timer()
     print("---------------------------------------------------")
     print("HEAPSORT:\n")
+    start_time = timer()
     n = len(laptops)
     i = n // 2
     for i in range(n, -1, -1):
